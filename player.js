eb9a77ee0657b93d37718456f6b71a91045ac12a
0ddaniels <42749711+0ddaniels@users.noreply.github.com>
2019-01-17 13:57:43 -0600

    Add files via upload

--- player.js
+++ player.js
@@ -20,15 +20,67 @@ let p2Stats = {
   sp: 60
 }
 
-let battle = {
-
-  attack: function(){
-    
+let p1battle = {
+// not finished
+  attack: function(mv){
+    if (mv == 1){
+      return this.moves.punch();
+    }
+    else if (mv == 2){
+      return this.moves.other;
+    }
   },
-  defend: function(){}
+  defend: function(atkmv){
+    let rawDamage = atkmv - this.stats.def;
+    this.stats.hp = this.stats.hp - rawDamage;
+    if (this.stats.hp < 0){
+      this.stats.hp = 0;
+    }
+  }
+}
+let physical = {
+  razorPunch: function(){
+    let baseDamage = this.stats.atk*0.75;
+    let bonus = this.stats.atk*0.5*Math.random();
+    return baseDamage + bonus;
+  },
+  charge: function(){
+    let pain = Math.random;
+    let bonus = this.stats.atk*0.5*Math.random();
+    if (pain < 0.5){
+      this.stats.hp - bonus/2;
+      return this.stats.atk+bonus;
+    }
+    else{
+      return this.stats.atk+bonus;
+    }
+  },
+  roundhouseKick: function(){
+    
+  }
 
 }
-
+let magic = {
+  hydroPump: function(){
+    if (this.stats.mp >= 8){
+      this.stats.mp = this.stats.mp - 8;
+      let tetradice = Math.random;
+      let pump = math.ceil(this.stats.sp/3);
+      if (tetradice <0.10){
+        return pump;
+      }
+      else if (tetradice <0.30){
+        return 2*pump;
+      }
+      else if (tetradice <0.60){
+        return 3*pump;
+      }
+      else{
+        return 4*pump;
+      }
+    }
+  }
+}
 
 
 
